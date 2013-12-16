645ea147f09de8fc733d24bf8618875b0bc6c941
Chester Kener <Cl3Kener@gmail.com>
2013-12-16 02:23:35 -0500

    Remove Compile Flag
    * Some Users may not have computer capable of this option.  Let's just leave it up to the computer to decide how fast it can build.
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- hkbuild.sh
+++ hkbuild.sh
@@ -75,7 +75,7 @@ rm -rf out/target/product/$DEVICE/*.zip
 rm -rf out/target/product/$DEVICE/*.zip.md5
 
 # Start Compilation
-script -q ~/HKCompile.log -c " make bacon -j16 "
+script -q ~/HKCompile.log -c " make bacon "
 echo -e ""
 
 # Show Elapsed Time
